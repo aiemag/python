3e2315ec58a449f8bd157616636d1b0f9ba366a8
lhh <lhh@localhost.localdomain>
2020-10-25 10:46:28 +0900

    changed

--- global_keyword.py
+++ global_keyword.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
-text = ""
+text = "hello"
+
+def read_text():
+    print(text)
+
+
+def append_text(add):
+    global text
+
+    text += add
+    print(text)
+
 
 def main():
-    global text
-    text += "hi"
-    print(text)
+    read_text()
+    append_text(" world")
 
 
 if __name__ == "__main__":
