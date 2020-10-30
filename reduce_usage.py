c879427e345d8e7750fc058a386724bfeec5a728
lhh <lhh@localhost.localdomain>
2020-10-30 23:09:09 +0900

    source changed

--- reduce_usage.py
+++ reduce_usage.py
@@ -2,13 +2,25 @@
 
 from functools import reduce
 
-def test_reduce():
-    rst = reduce(lambda x, y:x+y, range(1,11))
+def test_reduce1():
+    rst = reduce(lambda x, y: x+y, range(1,11))
+    print(rst)
+
+
+def test_reduce2():
+    rst = reduce(lambda x, y: x+y, range(1,11), 10)
+    print(rst)
+
+
+def test_reduce3():
+    rst = reduce(lambda x, y: x if x > y else y, range(1,11))
     print(rst)
 
 
 def main():
-    test_reduce()   
+    test_reduce1()   
+    test_reduce2()   
+    test_reduce3()   
 
 
 if __name__ == "__main__":
